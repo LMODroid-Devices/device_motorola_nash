da44f861897b6270be4b31da8d2696aa2e8b1f24
LMO GerritBot <gerrit@libremobileos.com>
2023-09-16 19:12:43 +0000

    Adapt to LMODroid by device-add script

--- lmodroid_nash.mk
+++ lmodroid_nash.mk
@@ -1,6 +1,6 @@
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Device
@@ -12,7 +12,7 @@ PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := nash
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := Moto Z (2)
-PRODUCT_NAME := lineage_nash
+PRODUCT_NAME := lmodroid_nash
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=nash \
